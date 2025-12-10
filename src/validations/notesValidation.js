3aa77b98cf96e81eaad269455989912955ce0480
Nadiia Savchuk <nadiia.savchuk.w@gmail.com>
2025-12-10 23:48:39 +0200

    add pagination, text search, and request validation for notes collection (#2)
    * add pagination, text search, and request validation for notes collection
    
    * update README

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,49 @@
+import { Joi, Segments } from 'celebrate';
+import { TAGS } from '../constants/tags.js';
+import { isValidObjectId } from 'mongoose';
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+const tagValidator = (value, helpers) => {
+  const normalized = TAGS.find(
+    (tag) => tag.toLowerCase() === value.toLowerCase(),
+  );
+  return !normalized ? helpers.error('any.only') : normalized;
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .insensitive()
+      .custom(tagValidator),
+    search: Joi.string().trim().allow(''),
+  }),
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }),
+};
+
+export const updateNoteSchema = {
+  ...noteIdSchema,
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }).min(1),
+};
