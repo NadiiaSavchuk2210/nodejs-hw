d0fd3f662972fba5272c68449e8b83811f5f5bcd
NadiiaSavchuk2210 <nadiia.savchuk.g@gmail.com>
2025-12-11 11:11:12 +0200

    implement user auth, sessions, and private notes

--- src/server.js
+++ src/server.js
@@ -5,17 +5,20 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import router from './routes/notesRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // Global middleware setup
-app.use(logger(), express.json(), cors());
+app.use(logger(), express.json(), cors(), cookieParser());
 
 // Register routes
-app.use(router);
+app.use(authRoutes);
+app.use(notesRoutes);
 
 // Fallback middlewares
 app.use(notFoundHandler);
